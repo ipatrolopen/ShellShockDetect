cd2b35ef6eb5967e823a4614023b80eaf1f9cb59
Roman Plášil <me@rplasil.name>
2014-09-26 13:07:28 +0800

    Initial Commit

---
+++ ShellShockDetect/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShellShockDetect
+//
+//  Created by Roman Plasil on 26/9/14.
+//  Copyright (c) 2014 ipatrol. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ShellShockDetectTests/ShellShockDetectTests.m
@@ -0,0 +1,40 @@
+//
+//  ShellShockDetectTests.m
+//  ShellShockDetectTests
+//
+//  Created by Roman Plasil on 26/9/14.
+//  Copyright (c) 2014 ipatrol. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface ShellShockDetectTests : XCTestCase
+
+@end
+
+@implementation ShellShockDetectTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
